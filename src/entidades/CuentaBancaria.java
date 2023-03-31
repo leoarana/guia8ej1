ac5b569e7c112985d39db7249d2eeb45436c492a
leoarana <leandroarana77@gmail.com>
2023-03-31 16:58:55 -0300

    commit inicial

---
+++ src/entidades/CuentaBancaria.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entidades;
+
+/**
+ *
+ * @author leandro
+ */
+public class CuentaBancaria {
+    //numeroCuenta(entero), dniCliente(entero largo), saldoActual
+    
+    private int numeroCuenta;
+    private long dniCliente;
+    private double saldoActual;
+
+    public CuentaBancaria() {
+    }
+
+    public int getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(int numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public long getDniCliente() {
+        return dniCliente;
+    }
+
+    public void setDniCliente(long dniCliente) {
+        this.dniCliente = dniCliente;
+    }
+
+    public double getSaldoActual() {
+        return saldoActual;
+    }
+
+    public void setSaldoActual(double saldoActual) {
+        this.saldoActual = saldoActual;
+    }
+
+    @Override
+    public String toString() {
+        return "CuentaBancaria{" + "numeroCuenta=" + numeroCuenta + ", dniCliente=" + dniCliente + ", saldoActual=" + saldoActual + '}';
+    }
+    
+    
+}
